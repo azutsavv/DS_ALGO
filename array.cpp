729aef530b1cf4eb169ea4e8e13463d61940a57d
Utsav Singh <azutsav2002@gmail.com>
2023-05-29 20:17:02 +0530

    array problem

---
+++ array.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int A[5]={1,2,3,4,5};
+    cout<<2[A]<<endl;
+    cout<<A[3]<<endl;
+    cout<<*(A+1);   // using pointer arithmetic
+    return 0;
+}
---
+++ array_representation.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ int A[5]={1,2,3,4,5};
+ cout<<A[1]<<endl;
+ cout<<1[A]<<endl;
+ cout<<*(A+1)<<endl;
+
+ return 0;   
+}
